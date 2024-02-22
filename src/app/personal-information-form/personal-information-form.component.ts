5c759262c77a99db09fb734ce21fb38e2f6aabd6
neezystudio <victor.ndirangu@kyosk.app>
2024-02-22 14:59:11 +0300

    form

--- src/app/personal-information-form/personal-information-form.component.ts
+++ src/app/personal-information-form/personal-information-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Injectable, inject } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
 
 @Component({
   selector: 'app-personal-information-form',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-information-form.component.scss']
 })
 export class PersonalInformationFormComponent implements OnInit {
+  formData = {
+    name: '',
+    email: ''
+  };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private apollo: Apollo) { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  submitForm() {
+    const mutation = gql`
+      mutation SubmitFormData($name: String!, $email: String!) {
+        submitFormData(name: $name, email: $email) {
+          success
+          message
+        }
+      }
+    `;
+
+    this.apollo.mutate({
+      mutation,
+      variables: {
+        name: this.formData.name,
+        email: this.formData.email
+      }
+    }).subscribe((result: any) => {
+      console.log('Form submission result:', result);
+      // Handle response here
+    }, (error: any) => {
+      console.error('Error submitting form:', error);
+    });
+  }
 }
+
+
