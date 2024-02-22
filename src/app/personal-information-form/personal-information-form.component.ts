073dbe2c26c7eb37954404a5afe3a082e6343676
neezystudio <victor.ndirangu@kyosk.app>
2024-02-22 13:19:03 +0300

    wip

--- src/app/personal-information-form/personal-information-form.component.ts
+++ src/app/personal-information-form/personal-information-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-personal-information-form',
   templateUrl: './personal-information-form.component.html',
-  styleUrls: ['./personal-information-form.component.css']
+  styleUrls: ['./personal-information-form.component.scss']
 })
 export class PersonalInformationFormComponent implements OnInit {
 
---
+++ src/mutations/graphql.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GraphqlService {
+
+  constructor(private apollo: Apollo) { }
+
+  submitForm(formData: any) {
+    return this.apollo.mutate({
+      mutation: gql`
+        mutation SubmitForm($name: String!, $email: String!) {
+          submitForm(name: $name, email: $email) {
+            success
+          }
+        }
+      `,
+      variables: {
+        name: formData.name,
+        email: formData.email
+      }
+    });
+  }
+}
