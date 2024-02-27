5fea2f3dbbeb9f1452150617bae66e82feae9c56
neezystudio <victor.ndirangu@kyosk.app>
2024-02-27 08:21:42 +0300

    namelist component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { appRoutes} from '../app.routes'
 
 import { AppComponent } from './app.component';
 import { PersonalInformationFormComponent } from './personal-information-form/personal-information-form.component';
 import { ApolloModule } from 'apollo-angular';
+import {provideRouter, RouterModule, Routes} from '@angular/router';
+import { NameListComponent } from './name-list/name-list.component';
+
+
 
 @NgModule({
   declarations: [	
     AppComponent,
-      PersonalInformationFormComponent
+      PersonalInformationFormComponent,
+      NameListComponent
    ],
   imports: [
     BrowserModule,
     FormsModule,
-    ApolloModule
+    ApolloModule,
+    RouterModule.forRoot(appRoutes, {
+      initialNavigation: 'enabledNonBlocking',
+      onSameUrlNavigation: 'reload'
+    }),
   ],
-  providers: [],
+  providers: [
+    //add the provider router line here
+    provideRouter(appRoutes)
+  ],
   bootstrap: [PersonalInformationFormComponent]
 })
 export class AppModule { }
---
+++ src/app/name-list/name-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-list',
+  templateUrl: './name-list.component.html',
+  styleUrls: ['./name-list.component.scss']
+})
+export class NameListComponent {
+
+}
