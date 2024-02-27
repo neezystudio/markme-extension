5129bd869b84aa9df225c31d6db06240cbb47869
neezystudio <victor.ndirangu@kyosk.app>
2024-02-27 15:08:57 +0300

    Hero service

--- src/app/name-list/name-list.component.ts
+++ src/app/name-list/name-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Hero } from '../Hero/hero';
 
 @Component({
   selector: 'app-name-list',
   templateUrl: './name-list.component.html',
   styleUrls: ['./name-list.component.scss']
 })
-export class NameListComponent {
+export class NameListComponent  implements OnInit{
+  heroes$!: Observable<Hero[]>;
+  selectedId = 0;
+
+  constructor(
+    private service: HeroService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.heroes$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        this.selectedId = parseInt(params.get('id')!, 10);
+        return this.service.getHeroes();
+      })
+    );
+  }
 
 }
+
+
